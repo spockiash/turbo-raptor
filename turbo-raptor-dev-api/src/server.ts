1334948d3039d6af68f206b2003b49bcde52f7b0
Bobo <spocky@post.cz>
2025-02-10 22:57:23 +0100

    added oak routing

--- turbo-raptor-dev-api/src/server.ts
+++ turbo-raptor-dev-api/src/server.ts
@@ -2,6 +2,7 @@ import { Application, Router, Context } from "@oak/oak";
 import chalk from "chalk";
 import figlet from "figlet";
 import { displayBanner, displayStartupMessage } from "./consoleUtils.ts"
+import { generateFakeStockData } from "./dataService.ts"
 
 const PROTOCOL:string = "HTTP"
 const PORT: number = 8000;
@@ -9,3 +10,14 @@ const HOST: string = "localhost";
 
 displayBanner();
 displayStartupMessage(PROTOCOL, HOST, PORT);
+
+const router = new Router()
+    .get("/api/fakeData", (ctx) => {
+        ctx.response.body = generateFakeStockData("ETHUSDT", "1h");
+    });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ hostname: HOST, port: PORT });
